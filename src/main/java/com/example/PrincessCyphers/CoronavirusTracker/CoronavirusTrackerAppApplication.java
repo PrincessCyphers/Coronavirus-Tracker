8c05ef0cba91be8e18c04bd0c1661ad8def36512
PrincessMav <princess-iza@hotmail.co.uk>
2021-11-17 00:05:38 +0000

    Covid-19 Tracker App

---
+++ src/main/java/com/example/PrincessCyphers/CoronavirusTracker/CoronavirusTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.PrincessCyphers.CoronavirusTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PrincessCyphers/CoronavirusTracker/CoronavirusTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PrincessCyphers.CoronavirusTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

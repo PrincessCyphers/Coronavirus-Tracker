08aedef23dc63be4a0cdc8462f443be446551240
PrincessMav <princess-iza@hotmail.co.uk>
2021-11-17 16:14:24 +0000

    area stats arrayed and instances created to populate list data when requested. newStats allows concurrency

--- src/main/java/com/example/PrincessCyphers/CoronavirusTracker/services/CoronavirusDataService.java
+++ src/main/java/com/example/PrincessCyphers/CoronavirusTracker/services/CoronavirusDataService.java
@@ -1,5 +1,6 @@
 package com.example.PrincessCyphers.CoronavirusTracker.services;
 
+import com.example.PrincessCyphers.CoronavirusTracker.models.AreaStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,6 +13,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.lang.System.in;
 
@@ -22,11 +25,14 @@ public class CoronavirusDataService {
 
     public static String Virus_Data_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<AreaStats> allStats = new ArrayList<>();           // array of area stats. to drop previous stats and recon with new data.
+
     @PostConstruct
 
     // to create schedule that allow prog to run on set timing, sec, min, day, month, year. setting to check at 6am every day
     @Scheduled(cron = "* * 6  * *")
     public void fetchViralData() throws IOException, InterruptedException {
+        List<AreaStats> newStats = new ArrayList<>();           // allows high user worklaod with no error responses whilst constructing new list-, creates instances of newStats and populates allStats above,
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(Virus_Data_URL))
@@ -36,9 +42,16 @@ public class CoronavirusDataService {
         StringReader csvBodyReader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-            System.out.println(state);
+            AreaStats areaStat = new AreaStats();
+            areaStat.setState(record.get("Province/State"));
+            areaStat.setCountry(record.get("Country/Region"));
+            areaStat.setLatestNUmberOfCases(Integer.parseInt(record.get(record.size()-1)));         //to access the latest column w/ latest data
+            System.out.println(areaStat);
+            newStats.add(areaStat);
+
         }
+
+        this.allStats = newStats;
 
     }
 

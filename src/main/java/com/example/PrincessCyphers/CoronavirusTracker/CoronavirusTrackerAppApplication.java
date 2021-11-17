29577ee4e47c016142ea96df81e7afce7c62be90
PrincessMav <princess-iza@hotmail.co.uk>
2021-11-17 15:36:17 +0000

    created data service now has timing, so data is fetched daily at 6

--- src/main/java/com/example/PrincessCyphers/CoronavirusTracker/CoronavirusTrackerAppApplication.java
+++ src/main/java/com/example/PrincessCyphers/CoronavirusTracker/CoronavirusTrackerAppApplication.java
@@ -2,8 +2,10 @@ package com.example.PrincessCyphers.CoronavirusTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronavirusTrackerAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/PrincessCyphers/CoronavirusTracker/services/CoronavirusDataService.java
+++ src/main/java/com/example/PrincessCyphers/CoronavirusTracker/services/CoronavirusDataService.java
@@ -1,13 +1,19 @@
 package com.example.PrincessCyphers.CoronavirusTracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
+import static java.lang.System.in;
 
 @Service
 public class CoronavirusDataService {
@@ -17,14 +23,25 @@ public class CoronavirusDataService {
     public static String Virus_Data_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct
+
+    // to create schedule that allow prog to run on set timing, sec, min, day, month, year. setting to check at 6am every day
+    @Scheduled(cron = "* * 6  * *")
     public void fetchViralData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(Virus_Data_URL))
                 .build();
-        HttpResponse httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
 
     }
-}
+
+    }
+
 

d2b65576dcbc45e9d5ff99b88f0c20acf9eac838
PrincessMav <princess-iza@hotmail.co.uk>
2021-11-17 23:47:11 +0000

    Created total of cases instance

--- src/main/java/com/example/PrincessCyphers/CoronavirusTracker/controllers/HomeController.java
+++ src/main/java/com/example/PrincessCyphers/CoronavirusTracker/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.example.PrincessCyphers.CoronavirusTracker.controllers;
 
+import com.example.PrincessCyphers.CoronavirusTracker.models.AreaStats;
 import com.example.PrincessCyphers.CoronavirusTracker.services.CoronavirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller             // Controller type selected allows rendering of HTML ui
 public class HomeController {
@@ -15,7 +18,10 @@ public class HomeController {
     @GetMapping ("/")               //when there's mapping to root directory, then call home.
     // modelling allows data to be set in relevant context of what's rendering the page. for here, it's HTML
     public String home(Model model) {
-        model.addAttribute("areaStats", coronavirusDataService.getAllStats());
+        List<AreaStats> allStats = coronavirusDataService.getAllStats();
+        int totalOfCases = allStats.stream().mapToInt(stat -> stat.getLatestNumberOfCases()).sum();
+        model.addAttribute("areaStats", allStats);
+        model.addAttribute("totalOfCases", totalOfCases);
         return "home";
     }
 }
